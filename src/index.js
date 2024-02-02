e51dc4925acbd91723e8623d46e778f1288b860e
Jacob Ye <gmding777@gmail.com>
2024-02-02 14:46:57 -0800

    testing eslint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import "./style.css";
 import { test, newDiv } from "./newscript";
+
 const buildContainer = () => {
     const element = document.createElement("div");
     element.classList.add("container");
-    element.classList.add("padding-10x")
+    element.classList.add("padding-10x");
     return element;
 };
 
 const buildGenericContent = (...content) => {
     const container = buildContainer();
-    content.forEach(text => {
+    content.forEach((text) => {
         const p = document.createElement("p");
         p.classList.add("text-content");
         p.innerHTML = text;
@@ -17,18 +18,18 @@ const buildGenericContent = (...content) => {
     });
 
     return container;
-}
+};
 
 const buttons = document.querySelectorAll("button");
-buttons.forEach(button => {
+buttons.forEach((button) => {
     button.addEventListener("click", () =>{
         console.log("clicked");
     });
 });
 
-let contentDiv = document.querySelector("#content");
+const contentDiv = document.querySelector("#content");
 const content1 = buildGenericContent("test1", "test2", "test3");
 const content2 = buildGenericContent("hello", "goodbye", "yes!");
 test();
-contentDiv.replaceChildren(content1,content2);
-contentDiv.appendChild(newDiv())
+contentDiv.replaceChildren(content1, content2);
+contentDiv.appendChild(newDiv());
